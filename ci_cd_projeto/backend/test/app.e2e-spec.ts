0aa491e401b5865e1e1ccb161d10ca5fa78acb31
Rafael Alexandre Piemontez <rafaelpiemontez@gmail.com>
2025-05-25 19:02:13 -0300

    Update app.e2e-spec.ts

--- ci_cd_projeto/backend/test/app.e2e-spec.ts
+++ ci_cd_projeto/backend/test/app.e2e-spec.ts
@@ -21,4 +21,11 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World a!');
+  });
 });
